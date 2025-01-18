a4acb3935a344236cec9b47e99de4a3fabedeed7
Leong Kit Ye <sherylleongkitye@gmail.com>
2025-01-19 01:05:34 +0800

    removed the placeholder team input

--- job-app-game/steps/Step1Basics.jsx
+++ job-app-game/steps/Step1Basics.jsx
@@ -10,15 +10,6 @@ const Step1Basics = ({ formData, updateFormData, updateValidation }) => (
       onChange={(value) => updateFormData('email', value)}
       value={formData.email}
     />
-    <LongTextInputField
-      number={1}
-      question="There is no 'I' in 'Team'! Introduce yourself without using the letter 'I'."
-      validateFunc={(val) => !val.toLowerCase().includes('i') && val.trim().length > 0}
-      errMsg="No 'i's allowed in your introduction (there's no 'i' in team!)"
-      onValidation={(isValid) => updateValidation('introduction', isValid)}
-      onChange={(value) => updateFormData('introduction', value)}
-      value={formData.introduction}
-    />
     <AboutYourselfNoI
       onValidation={(isValid) => updateValidation('introduction', isValid)}
       onChange={(value) => updateFormData('introduction', value)}
